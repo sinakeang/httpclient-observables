83399b200f13482caa7d9bee706c139180974d66
Sina Keang <sina.keang@innovapost.com>
2018-06-17 20:23:28 -0400

    add get request for sinakeang json file in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://api.github.com/users/sinakeang')
+      .subscribe(data => {
+        console.log(data);
+      })
+  }
 }
