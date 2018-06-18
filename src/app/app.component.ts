febe2284a3455882c7394283bcd2bc6ca9a03a56
Sina Keang <sina.keang@innovapost.com>
2018-06-17 20:29:13 -0400

    convert get response to UserReponse type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface UserResponse {
+  login: string,
+  bio: string,
+  company: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +18,11 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get('https://api.github.com/users/sinakeang')
+    this.http.get<UserResponse>('https://api.github.com/users/sinakeang')
       .subscribe(data => {
-        console.log(data);
+        console.log("User Login: " + data.login);
+        console.log("User Bio: " + data.bio);
+        console.log("User Company: " + data.company);
       })
   }
 }
